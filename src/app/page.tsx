b7c031bafaafa1155048e4d3f60330f2c333b3ec
AkashAkter <akash31akter@gmail.com>
2025-03-21 00:04:20 +0600

    Changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
         Button
       </button>
       <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-        Button
+        Click Here
       </button>
     </div>
   );
